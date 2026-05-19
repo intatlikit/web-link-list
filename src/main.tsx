7c269a71d2dd1dd55c406c3adc45228ffd47eb0f
Intat Likitpolchaloon <i.likitpolchaloon@accenture.com>
2026-05-19 16:07:09 +0700

    chore: test email notification again after secret update

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
 
-// Deployment notification test
+// Deployment notification test v2
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
