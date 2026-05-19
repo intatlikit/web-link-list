07279a90426a6506bc601c769bdcde5aa099354c
Intat Likitpolchaloon <i.likitpolchaloon@accenture.com>
2026-05-19 15:58:17 +0700

    chore: trigger deployment test for email notification

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
 
+// Deployment notification test
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
